5d230a6c9197a3decf492e6ca07d6b866f75f20b
joaquin de los reyes <joaquindr2011@gmail.com>
2021-05-11 18:36:38 -0300

    mere con development

--- src/pages/Documentacion.js
+++ src/pages/Documentacion.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Documentacion = () => {
+  const [guardado, setGuardado] = useState(false);
+
+  const guardarArchivo = () => {
+    setGuardado(true);
+    setTimeout(() => {
+      setGuardado(false);
+    }, [5000]);
+  };
+
   return (
     <>
       <div
@@ -53,19 +62,35 @@ const Documentacion = () => {
                   </div>
                 </div>
                 <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-                  <button className="btn btn-primary" type="button">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-file-earmark-diff-fill"
-                      viewBox="0 0 16 16"
+                  <div class="mb-3">
+                    <input
+                      className="form-control mt-3 ms-2"
+                      type="file"
+                      id="formFile"
+                    />
+                    <button
+                      className="btn btn-primary ms-2"
+                      type="button"
+                      onClick={() => guardarArchivo()}
                     >
-                      <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM8 6a.5.5 0 0 1 .5.5V8H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V9H6a.5.5 0 0 1 0-1h1.5V6.5A.5.5 0 0 1 8 6zm-2.5 6.5A.5.5 0 0 1 6 12h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5z" />
-                    </svg>{" "}
-                    Agregar
-                  </button>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-file-earmark-diff-fill"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M9.293 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V4.707A1 1 0 0 0 13.707 4L10 .293A1 1 0 0 0 9.293 0zM9.5 3.5v-2l3 3h-2a1 1 0 0 1-1-1zM8 6a.5.5 0 0 1 .5.5V8H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V9H6a.5.5 0 0 1 0-1h1.5V6.5A.5.5 0 0 1 8 6zm-2.5 6.5A.5.5 0 0 1 6 12h4a.5.5 0 0 1 0 1H6a.5.5 0 0 1-.5-.5z" />
+                      </svg>
+                      Agregar
+                    </button>
+                    {guardado && (
+                      <>
+                        <p>El archivo se guardo correctamente</p>
+                      </>
+                    )}
+                  </div>
                 </div>
               </div>
               <div className="col mt-3">
