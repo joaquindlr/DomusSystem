94347501b1bf57751ee214ed23aa27b2f37b7387
joaquin de los reyes <joaquindr2011@gmail.com>
2021-05-11 16:29:39 -0300

    se termino la descripcion de la propiedad

--- src/pages/Propiedad.js
+++ src/pages/Propiedad.js
@@ -19,10 +19,13 @@ const Propiedad = () => {
   }, [casa]);
 
   return (
-    <>
+    <div style={{ backgroundColor: "#E2F0FF", paddingBottom: "2rem" }}>
       <div
-        className="d-md-flex md-justify-content-center py-5 px-3 px-md-5"
-        style={{ backgroundColor: "#E2F0FF" }}
+        className="d-md-flex justify-content-center py-5 px-3 px-md-5"
+        style={{
+          display: "grid",
+          placeItems: "center",
+        }}
       >
         <div className="w-100" style={{ maxWidth: "50rem" }}>
           {casa && <img className="rounded-3 w-100 " src={casa.imagen} />}
@@ -40,7 +43,7 @@ const Propiedad = () => {
                   placeItems: "center",
                 }}
               >
-                <div>
+                <div className="">
                   <p className="fs-5 fw-bold">Fecha propuesta: </p>
                   <input type="date" className="form-control" />
                   <p className="fs-5 fw-bold mt-4">Mensaje: </p>
@@ -71,7 +74,99 @@ const Propiedad = () => {
           )}
         </div>
       </div>
-    </>
+      <div
+        style={{
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <div style={{ width: "80%" }} className="card p-3">
+          <h3>Descripcion</h3>
+          <p>
+            Descripcion is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+          <h5 className="mt-4">Detalles</h5>
+          <div className="d-md-flex">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col"></th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">Cod propiedad:</th>
+                  <th scope="row">123456</th>
+                </tr>
+                <tr>
+                  <th scope="row">Condicion:</th>
+                  <th scope="row">Alquiler</th>
+                </tr>
+                <tr>
+                  <th scope="row">Tipo:</th>
+                  <th scope="row">Casa</th>
+                </tr>
+                <tr>
+                  <th scope="row">Dormitorios:</th>
+                  <th scope="row">4</th>
+                </tr>
+                <tr>
+                  <th scope="row">Baños:</th>
+                  <th scope="row">2</th>
+                </tr>
+                <tr>
+                  <th scope="row">Lavadero:</th>
+                  <th scope="row">Si</th>
+                </tr>
+              </tbody>
+            </table>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col"></th>
+                  <th scope="col"></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">Muebles:</th>
+                  <th scope="row">Amoblado</th>
+                </tr>
+                <tr>
+                  <th scope="row">Terraza:</th>
+                  <th scope="row">No</th>
+                </tr>
+                <tr>
+                  <th scope="row">Cocina:</th>
+                  <th scope="row">Si</th>
+                </tr>
+                <tr>
+                  <th scope="row">Capacidad:</th>
+                  <th scope="row">6 personas</th>
+                </tr>
+                <tr>
+                  <th scope="row">Patio:</th>
+                  <th scope="row">Si</th>
+                </tr>
+                <tr>
+                  <th scope="row">Garage:</th>
+                  <th scope="row">Si</th>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
