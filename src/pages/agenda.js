a8d358677a5751a1ffc7875aafc483353de373fc
joaquin de los reyes <joaquindr2011@gmail.com>
2021-05-12 16:40:03 -0300

    se ARREGLO el login y se agregaron las validaciones

--- src/pages/agenda.js
+++ src/pages/agenda.js
@@ -1,62 +1,241 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "../components/Modal";
 
-const agenda = () => {
+const Agenda = () => {
+  const [visible, setVisible] = useState(false);
+  const handleVisible = () => {
+    setVisible(false);
+  };
+
   return (
     <>
-    <div class="container">
-          <div class="row">
-              <div class="col-auto">
-                  <input type="date" className="form-control input" />
-                  <br/><br/><br/><br/><br/><br/>
-                  <div class="row">
-                  <button type="button" class="btn btn-secondary btn-sm" >Filtrar por Propiedades</button>
-                  </div>
-                  <br/>
-                  <div class="row">
-                  <button type="button" class="btn btn-secondary btn-sm">Filtrar por Agentes</button>
-                  </div>
-                  <br/>
-                  <div class="row">
-                  <button type="button" class="btn btn-secondary btn-sm">Agendar Cita</button>
-                  </div>
-              </div>
-              <div class="col">
-              <table class="table table-bordered table-striped   " >
-                 <thead> 
-                 <th scope="col"></th>
-                 <th scope="col" >Dom</th>
-                  <th scope="col">Lun</th>
-                  <th scope="col">Mar</th>
-                  <th scope="col">Mie</th>
-                  <th scope="col">Jue</th>
-                  <th scope="col">Vie</th>
-                  <th scope="col">Sab</th>
-                  </thead>
-                  <tbody>
-                    <tr><th scope="row">7:00</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
-                    <tr> <th scope="row">8:00</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
-                    <tr> <th scope="row">9:00</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
-                    <tr> <th scope="row">10:00</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
-                    <tr> <th scope="row">11:00</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
-                    <tr> <th scope="row">12:00</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
-                    <tr> <th scope="row">13:00</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
-                    <tr> <th scope="row">14:00</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
-                    <tr> <th scope="row">15:00</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
-                    <tr> <th scope="row">16:00</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
-                    <tr> <th scope="row">17:00</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
-                    <tr> <th scope="row">18:00</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
-                    <tr> <th scope="row">19:00</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
-                    <tr> <th scope="row">20:00</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
-                    <tr> <th scope="row">21:00</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
-                    <tr> <th scope="row">22:00</th><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
-                  </tbody>
-                 </table>
-                 </div>
-
-              </div>
-              </div>
-
+      <div classNameName="container">
+        <Modal visible={visible} handleVisible={handleVisible} />
+        <div className="row">
+          <div className="col-auto">
+            <input type="date" classNameName="form-control input" />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <div className="row">
+              <button
+                onClick={() => setVisible(true)}
+                type="button"
+                className="btn btn-secondary btn-sm"
+              >
+                Filtrar por Propiedades
+              </button>
+            </div>
+            <br />
+            <div className="row">
+              <button type="button" className="btn btn-secondary btn-sm">
+                Filtrar por Agentes
+              </button>
+            </div>
+            <br />
+            <div className="row">
+              <button type="button" className="btn btn-secondary btn-sm">
+                Agendar Cita
+              </button>
+            </div>
+          </div>
+          <div className="col">
+            <table className="table table-bordered table-striped   ">
+              <thead>
+                <th scope="col"></th>
+                <th scope="col">Dom</th>
+                <th scope="col">Lun</th>
+                <th scope="col">Mar</th>
+                <th scope="col">Mie</th>
+                <th scope="col">Jue</th>
+                <th scope="col">Vie</th>
+                <th scope="col">Sab</th>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">7:00</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  {" "}
+                  <th scope="row">8:00</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  {" "}
+                  <th scope="row">9:00</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  {" "}
+                  <th scope="row">10:00</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  {" "}
+                  <th scope="row">11:00</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  {" "}
+                  <th scope="row">12:00</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  {" "}
+                  <th scope="row">13:00</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  {" "}
+                  <th scope="row">14:00</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  {" "}
+                  <th scope="row">15:00</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  {" "}
+                  <th scope="row">16:00</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  {" "}
+                  <th scope="row">17:00</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  {" "}
+                  <th scope="row">18:00</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  {" "}
+                  <th scope="row">19:00</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  {" "}
+                  <th scope="row">20:00</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  {" "}
+                  <th scope="row">21:00</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  {" "}
+                  <th scope="row">22:00</th>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
-export default agenda;
+export default Agenda;
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,30 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-    return (
-    <>
-            <div className="box">
-                <div className="container">
-                    <div className="form">
-                        <h2>Ingresar</h2>
-                        <form>
-                            <div className="inputBox">
-                                <input type="text" placeholder="Usuario"/>
-                            </div>
-                            <div className="inputBox">
-                                <input type="password" placeholder="Contrasena"/>
-                            </div>
-                            <div className="inputBox">
-                                <input type="submit" value="Registrarse"/>
-                            </div>
-                            <p className="forget">Olvidaste tu Contrasena ? <a href="#">Haz click Aquí</a></p>
-                            <p className="forget">No tenes una cuenta ? <a href="#">Registrate</a></p>
-                        </form>
-                    </div>
-                </div>
-            </div>
+  const { register, handleSubmit } = useForm();
 
-    </>
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [errorPassword, setErrorPassword] = useState(false);
+
+  const onSubmit = (data) => {
+    validar(data);
+  };
+
+  const validar = (data) => {
+    console.log("asdasd", data.contraseña, data.email, data);
+    if (data.contraseña == "") {
+      setErrorPassword(true);
+    } else {
+      setErrorPassword(false);
+    }
+    if (data.email == "") {
+      setErrorEmail(true);
+    } else {
+      setErrorEmail(false);
+    }
+  };
+
+  return (
+    <div
+      style={{
+        paddingTop: "4rem",
+        paddingBottom: "4rem",
+        display: "grid",
+        placeItems: "center",
+        backgroundColor: "#f2f2f2",
+        paddingTop: "2rem",
+      }}
+    >
+      <div
+        className="rounded-3 py-5 shadow "
+        style={{
+          width: "30rem",
+          height: "30rem",
+          display: "grid",
+          placeItems: "center",
+          backgroundColor: "white",
+        }}
+      >
+        <div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div class="mb-3">
+              <label for="email" class="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                class="form-control"
+                id="email"
+                defaultValue=""
+                name="email"
+                {...register("email")}
+              />
+              {errorEmail && <p>El email esta vacio</p>}
+            </div>
+            <div class="mb-3">
+              <label for="contraseña" class="form-label">
+                Contraseña
+              </label>
+              <input
+                type="password"
+                class="form-control"
+                name="contraseña"
+                id="contraseña"
+                {...register("contraseña")}
+              />
+              {errorPassword && <p>La contraseña esta vacia</p>}
+            </div>
+            <button type="submit" class="btn btn-primary">
+              Iniciar sesion
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
