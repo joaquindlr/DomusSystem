011272b4652e88dcd1efa320b4feeff3be20b486
TomasVilalta <tomasfedericovilalta@gmail.com>
2021-05-14 15:20:47 -0300

    Update Header.js

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -40,18 +40,9 @@ function Header() {
 
 //manejo de navbar para responsiveness
   const [click, setClick] = useState(false);
-  // const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
-
-  // const showButton = () => {
-  //   if (window.innerWidth <= 960) {
-  //     setButton(false);
-  //   } else {                 Esto de aca no sirve para nada pero bueno aca queda por las dudas
-  //     setButton(true);
-  //   }
-  // };
 
   return (
     <>  
@@ -72,86 +63,28 @@ function Header() {
                 </Link>
               </li>
               <li className='nav-item'>
-                <Link
-                  to='/nosotros-info'
-                  className='nav-links'
-                  onClick={closeMobileMenu}
-                >
-                  Nosotros
+                <Link to='/nosotros-info' className='nav-links' 
+                onClick={closeMobileMenu}>
+                Nosotros
                 </Link>
               </li>
               <li className='nav-item'>
-                <Link
-                  to='/q-and-a'
-                  className='nav-links'
-                  onClick={closeMobileMenu}
-                >
+                <Link to='/q-and-a' className='nav-links'
+                 onClick={closeMobileMenu}>
                   Contacto
                 </Link>
               </li>
-              {/* <li className='nav-btn'>
-                {button ? (
-                  <Link to='/login' className='btn-link'>
-                    <button type="button" class="btn btn-light">Secondary</button>
-                  </Link>
-                ) : (                                                               Esto de aca no sirve para nada pero bueno aca queda por las dudas
-                  <Link to='/sign-up' className='btn-link'>
-                    <Button
-                      buttonStyle='btn--primary' buttonColor="blue" 
-                      buttonSize='btn--mobile'
-                      onClick={() => setClick(false)}
-                    >
-                      Login
-                    </Button>
-                  </Link>
-                )}
-              </li> */}
-               
-                {/* <li>
-                <Link
-                  to='/Login'
-                  className='nav-links-hlight'
-                  onClick={closeMobileMenu}
-                >
-                  Login
-                </Link>
-              </li>
-              <li>
-                <Link
-                  to='/Registro'
-                  className='nav-links-hlight-secondary'
-                  onClick={closeMobileMenu}
-                >
-                  Register
-                </Link>
-              </li>
-                   <li>
-                   <Link
-                     to='/Menu-empleado'
-                     className='nav-links-hlight'
-                     onClick={closeMobileMenu}
-                   >
-                     Menú
-                   </Link>
-                 </li> */}
-
                  {!logeado ? (
           <>
              <li>
-                <Link
-                  to='/Login'
-                  className='nav-links-hlight'
-                  onClick={closeMobileMenu}
-                >
+                <Link to='/Login' className='nav-links-hlight'
+                onClick={closeMobileMenu}>
                   Login
                 </Link>
               </li>
               <li>
-                <Link
-                  to='/Registro'
-                  className='nav-links-hlight-secondary'
-                  onClick={closeMobileMenu}
-                >
+                <Link to='/Registro' className='nav-links-hlight-secondary' 
+                onClick={closeMobileMenu}>
                   Register
                 </Link>
               </li>
@@ -160,22 +93,16 @@ function Header() {
           <>
             {empleado && (
               <li>
-              <Link
-                to='/Menu-empleado'
-                className='nav-links-hlight'
-                onClick={closeMobileMenu}
-              >
+              <Link to='/Menu-empleado' className='nav-links-hlight'
+              onClick={closeMobileMenu}>
                 Menú
               </Link>
             </li>
             )}
             <li>
-                   <Link
-                     to='/'
-                     className='nav-links-hlight-secondary'
-                     onClick={closeMobileMenu, cerrarSesion}   /*El menu en móbil no se cierra cuando cerrás sesión no se por qué xd */
-                   >
-                     Cerrar sesión
+                   <Link to='/' className='nav-links-hlight-secondary'
+                    onClick={closeMobileMenu, cerrarSesion}   /*El menu en móbil no se cierra cuando cerrás sesión no se por qué xd */
+                   >Cerrar sesión
                    </Link>
                  </li>
           </>
